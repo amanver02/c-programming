191b96b3143dac474f5d83625f48268e55a0c143
amanver02 <155369942+amanver02@users.noreply.github.com>
2024-02-24 15:58:47 +0530

    Add files via upload

---
+++ Triangle Area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    float base, height, area;
+
+    printf("Enter the base and height of triangle :");
+    scanf("%f %f", &base, &height);
+
+    area = 0.5 * base * height;
+
+    printf("Area of triangle : %.2f", area);
+    return 0;
+}
